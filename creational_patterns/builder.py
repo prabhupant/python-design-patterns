d504fa31ec88de591bb65abc29bc294da9f822fa
Prabhu Pant <prabhupant09@gmail.com>
2021-05-10 18:02:45 +0530

    Add builder

---
+++ creational_patterns/builder.py
@@ -0,0 +1,8 @@
+"""
+Builder pattern is like factory pattern but it provides more flexibility.
+
+Good guide - https://stackabuse.com/creational-design-patterns-in-python/#builder
+
+Differences - https://stackoverflow.com/a/757773/6111200
+"""
+
